7db457bbde409fc97f461ad4d92bee60ff2144f1
davidwyt <davidwyt@outlook.com>
2014-02-06 16:52:15 +0800

    add db for android app

---
+++ VehicleApp/src/com/vehicle/app/db/DBHelper.java
@@ -0,0 +1,38 @@
+package com.vehicle.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	private static final String DATABASE_NAME = "VEHICLE.db";
+	private static final int DATABASE_VERSION = 1;
+
+	private static final String DATABASE_TABLE_MESSAGE = "CREATE TABLE IF NOT EXIST `MESSAGE` (`ID` CHAR(40) PRIMARY KEY NOT NULL, `SOURCE` VARCHAR(255) NOT NULL, `TARGET` VARCHAR(255) NOT NULL, `CONTENT` TEXT NOT NULL, `SENTTIME` INTEGER, `STATUS` CHAR(40));";
+	private static final String DATABASE_INDEX_MSGSOURCE = "CREATE INDEX `MSG_SOURCE_IDX` ON `MESSAGE`(`SOURCE` ASC);";
+	private static final String DATABASE_INDEX_MSGTARGET = "CREATE INDEX `MSG_TARGET_IDX` ON `MESSAGE`(`TARGET` ASC);";
+
+	public DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.beginTransaction();
+		db.execSQL(DATABASE_TABLE_MESSAGE);
+		db.execSQL(DATABASE_INDEX_MSGSOURCE);
+		db.execSQL(DATABASE_INDEX_MSGTARGET);
+		db.setTransactionSuccessful();
+		db.endTransaction();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ VehicleApp/src/com/vehicle/app/db/DBManager.java
@@ -0,0 +1,65 @@
+package com.vehicle.app.db;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.vehicle.app.bean.Message;
+import com.vehicle.app.bean.MessageStatus;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBManager {
+
+	private DBHelper mDBHelper;
+	private SQLiteDatabase mDB;
+
+	private final static String SQL_MESSAGE_INSERT = "INSERT INTO `MESSAGE`(`ID`, `SOURCE`, `TARGET`, `CONTENT`, `SENTTIME`) VALUES(?, ?, ?, ?, ?)";
+	private final static String SQL_MESSAGE_SELECT = "SELECT `ID`, `SOURCE`, `TARGET`, `CONTENT`, `SENTTIME` FROM `MESSAGE` WHERE `SOURCE` = ? AND `TARGET` = ?";
+
+	public DBManager(Context context) {
+		mDBHelper = new DBHelper(context);
+		mDB = mDBHelper.getWritableDatabase();
+	}
+
+	public void addMessage(String id, String source, String target, String content, Date sentTime) {
+		mDB.execSQL(SQL_MESSAGE_INSERT, new Object[] { id, source, target, content, sentTime });
+	}
+
+	public void addMessage(Message msg) {
+		mDB.execSQL(SQL_MESSAGE_INSERT, new Object[] { msg.getId(), msg.getSource(), msg.getTarget(), msg.getContent(),
+				msg.getSentDate() });
+	}
+
+	public List<Message> queryMessage(String source, String target) {
+		Cursor cursor = mDB.rawQuery(SQL_MESSAGE_SELECT, new String[] { source, target });
+
+		List<Message> messages = new ArrayList<Message>();
+		while (cursor.moveToNext()) {
+			Message msg = new Message();
+
+			msg.setId(cursor.getString(cursor.getColumnIndex("ID")));
+
+			msg.setSource(cursor.getString(cursor.getColumnIndex("SOURCE")));
+
+			msg.setTarget(cursor.getString(cursor.getColumnIndex("TARGET")));
+
+			msg.setContent(cursor.getString(cursor.getColumnIndex("CONTENT")));
+
+			long sentTime = cursor.getLong(cursor.getColumnIndex("SENTTIME"));
+			msg.setSentDate(new Date(sentTime));
+
+			msg.setStatus(MessageStatus.valueOf(cursor.getString(cursor.getColumnIndex("STATUS"))));
+
+			messages.add(msg);
+		}
+
+		return messages;
+	}
+
+	public void close() {
+		mDB.close();
+	}
+}
