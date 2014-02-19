2c56929c651e99f60b670e9b90dade257aad28c0
JerryBlight <qihong891009@gmail.com>
2014-02-19 15:55:32 +0800

    update

--- VehicleCommon/src/com/vehicle/service/bean/AddLocateRequest.java
+++ VehicleCommon/src/com/vehicle/service/bean/AddLocateRequest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.vehicle.service.bean;
 
 import java.util.Date;
@@ -38,32 +37,3 @@ public class AddLocateRequest implements IRequest{
 	}
 
 }
-=======
-package com.vehicle.service.bean;
-
-public class AddLocateRequest implements IRequest{
-	
-	private String id;
-	private double locateX;
-	private double locateY;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public double getLocateX() {
-		return locateX;
-	}
-	public void setLocateX(double locateX) {
-		this.locateX = locateX;
-	}
-	public double getLocateY() {
-		return locateY;
-	}
-	public void setLocateY(double locateY) {
-		this.locateY = locateY;
-	}
-
-}
->>>>>>> 873b6dca133d6876b5a1aa2e5a8409097dec413a
